aba6b0fe1d097a3aa5c458e0c5faab64065e4c06
Kaique-devSantos <kaiqueads.2023@gmail.com>
2024-01-05 22:36:43 -0300

    trabalhado com null  e undefined

---
+++ type-booleano.js
@@ -0,0 +1,16 @@
+const primeiroNumero = 5;
+const segundoNumero = alura;
+let cadastroAtivado = true;
+
+console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2)
+
+let input = null;
+let input2;
+
+console.log(input); // null
+console.log(input2); // undefined
