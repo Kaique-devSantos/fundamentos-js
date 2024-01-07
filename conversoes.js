5e05fcfe07d2923f916eac9fdb36f0c7f3d8b2fb
Kaique-devSantos <kaiqueads.2023@gmail.com>
2024-01-07 13:16:38 -0300

    criamos arquivo conversoes onde entendemos e trabalhamos conversoes de string e num

---
+++ conversoes.js
@@ -0,0 +1,16 @@
+
+//conversao implicita
+const numero = 456;
+const numeroString = "456"; 
+
+// console.log(numero + numeroString) desta forma concatena resultado: 456456 
+// console.log(numero === numeroString) false
+
+//Number()
+// string()
+console.log(numero + Number(numeroString));
+
+
+
+
+
