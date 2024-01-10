47e7ed1a808e9ea63550a3e4dcb1eef4ab29b2f0
Kaique-devSantos <kaiqueads.2023@gmail.com>
2024-01-10 15:04:25 -0300

    criando arquivo erros e console

---
+++ erros.js
@@ -0,0 +1,15 @@
+// const numero ; SyntaxError: missing initializer in const declaration, Em const o inicializador é valor que a passado
+
+
+const numero = 0;
+console.log(numero);
+
+// console.log(numero SyntaxError: missing ) after argument list
+
+// console.log(minhaVar) ReferenceError: minhaVar is not defined
+console.error(new Error("deu erro"));
+
+
+
+
+
