cfefc38b90eefc90f7d9e9284e5f3c41899fa3af
Kaique-devSantos <kaiqueads.2023@gmail.com>
2024-01-11 11:02:36 -0300

    criando arquivo expressoes-funcoes

---
+++ expressoes-funcoes.js
@@ -0,0 +1,10 @@
+// Declaracao de funcao
+// function minhaFuncao(param) {
+//     // bloco de código
+//   }
+  
+//   minhaFuncao("param")
+
+// expresao de funcao
+const soma = function(num1, num2) {return num1 + num2}
+console.log(soma(1, 1))
